06c5b2055a8af2188c9787766546704d5fd4656b
Ninja.69 <130167556+Ninja-69@users.noreply.github.com>
2023-10-17 12:24:46 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,6 +6,7 @@ require("@helpers/extenders/Message");
 require("@helpers/extenders/Guild");
 require("@helpers/extenders/GuildChannel");
 
+const { checkForUpdates } = require("@helpers/BotUtils");
 const { initializeMongoose } = require("@src/database/mongoose");
 const { BotClient } = require("@src/structures");
 const { validateConfiguration } = require("@helpers/Validator");
@@ -25,6 +26,9 @@ process.on("unhandledRejection", (err) => client.logger.error(`Unhandled excepti
 
 
 
+    // initialize the database
+  await initializeMongoose();
+  
 
   // start the client
   await client.login(process.env.BOT_TOKEN);
